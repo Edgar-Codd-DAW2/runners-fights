8e700101a9e919f494218bd44776b93f68ce475b
Rogercapdevila <72825138+Rogercapdevila@users.noreply.github.com>
2022-01-24 16:31:31 +0100

    Destruir bala al final de la animación

--- runners-fights/Assets/Scripts/BulletScript.cs
+++ runners-fights/Assets/Scripts/BulletScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// runners
 public class BulletScript : MonoBehaviour
 {
     public AudioClip sound;
     public float speed;
+   
 
     private Rigidbody2D rigidbody2D;
     private Vector2 direction;
@@ -13,19 +14,28 @@ public class BulletScript : MonoBehaviour
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+       
     }
         
     private void FixedUpdate()
     {
         if (direction == Vector2.left) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         else transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-
         rigidbody2D.velocity = direction * speed;
+        
+
+       
     }
 
     public void SetDirection(Vector2 direction)
     {
         this.direction = direction;
     }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+
 
 }
