fd10824c664acb9a9b7cd71f4d3cf08f90fc42bc
Kniro04 <79446818+Kniro04@users.noreply.github.com>
2022-02-27 21:04:00 +0100

    Multiplayer Connect to Map

--- runners-fights/Assets/Menu/LobbyManager.cs
+++ runners-fights/Assets/Menu/LobbyManager.cs
@@ -148,6 +148,6 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public void OnClickPlayButton()
     {
-        PhotonNetwork.LoadLevel("");
+        PhotonNetwork.LoadLevel("Stadiummulti1");
     }
 }
