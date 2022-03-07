d5fa8394c4736780c2b2801ef8730d0de497f047
Kniro04 <79446818+Kniro04@users.noreply.github.com>
2022-03-07 16:07:59 +0100

    Camera fix

--- runners-fights/Assets/Menu/PlayerSpawner.cs
+++ runners-fights/Assets/Menu/PlayerSpawner.cs
@@ -8,11 +8,14 @@ public class PlayerSpawner : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] playerPrefabs;
     public Transform[] spawnPoints;
+    //public GameObject sceneCamera;
+
     void Start()
     {
         int randomNumber = Random.Range(0, spawnPoints.Length);
         Transform spawnPoint = spawnPoints[randomNumber];
         GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playeAvatar"]];
         PhotonNetwork.Instantiate(playerToSpawn.name, spawnPoint.position, Quaternion.identity);
+        //sceneCamera.SetActive(false);
     }
 }
