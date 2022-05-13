7609b8e487db67a898877c1eeb1568fe9bf6b490
Kniro04 <79446818+Kniro04@users.noreply.github.com>
2022-05-13 19:32:44 +0200

    put to post

--- runners-fights/Assets/Scripts/Ranking.cs
+++ runners-fights/Assets/Scripts/Ranking.cs
@@ -29,31 +29,6 @@ public class Ranking : MonoBehaviour
 
         form.AddField("email", email);
 
-        /*//byte[] myData = System.Text.Encoding.UTF8.GetBytes(emai);
-        //UnityWebRequest www = UnityWebRequest.Put("http://127.0.0.1:8080/api/kills", myData);
-
-        Debug.Log("{\"email\":\"" + email + "\"}");
-
-        //UnityWebRequest www = UnityWebRequest.Put("http://127.0.0.1:8080/api/kills", "{\"email\":\"" + email + "\"}");
-        //www.SetRequestHeader("Content-Type", "application/json");
-
-        string str = "{\"email\":\"" + email + "\"}";
-        JObject json = JObject.Parse(str);
-        //byte[] myData = System.Text.Encoding.UTF8.GetBytes("{\"email\":\"" + email + "\"}");
-        byte[] myData = json.Properties().Select(p => (byte)p.Value).ToArray();*/
-
-        /*string str = "{\"email\":\"" + email + "\"}";
-        JObject json = JObject.Parse(str);
-        Debug.Log(json);
-        string jsonString = JsonUtility.ToJson(json) ?? "";
-        Debug.Log(jsonString);
-        byte[] myData = System.Text.Encoding.UTF8.GetBytes("admin@admin.com");
-        form.AddField("email", email);*/
-
-        //Debug.Log(form.data);
-        //byte[] rawData = form.data;
-
-        //Debug.Log(rawData);
         UnityWebRequest www = UnityWebRequest.Post("http://127.0.0.1:8080/api/kills", form);
         
         yield return www.Send();
